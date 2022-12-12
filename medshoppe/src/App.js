2642837948e245870a1f1076845f2e8915b3c8c0
Faizan7012 <frtoons7012@gmail.com>
2022-12-12 21:32:41 +0530

    new

---
+++ medshoppe/src/App.js
@@ -0,0 +1,14 @@
+import { Box, Flex, Image} from '@chakra-ui/react'
+import './App.css';
+
+function App() {
+  return (
+    <Box className="App">
+      <Flex justifyContent='center' alignItems='center'>
+      <Image src='https://i.ibb.co/s5mNPnz/1.png' />
+      </Flex>
+    </Box>
+  );
+}
+
+export default App;
