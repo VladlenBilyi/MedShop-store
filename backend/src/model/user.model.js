2f8e876ec3fe4d204ec365c02f518ca8aa0ffbb8
jeyjy <jeyjy0810@gmail.com>
2022-12-14 15:26:51 +0530

    signup & signin routes completed

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,8 +1,12 @@
 const { Schema, model } = require("mongoose");
+
 const userSchema = new Schema({
   name: { type: String},
-  username: { type: String, required: true },
-  email: { type: String, required: true,unique:true },
+  username: { type: String, required: true},
+  password : {type:String,required:true},
+  email: { type: String, required: true,unique:true,match:
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ },
+  userType:{type:String,enum:["user","admin"],default:"user"}
 });
 
 const userModel = model("user", userSchema);
